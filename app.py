872a9fb7ad59ab870919386ba86c4af8e77e8301
AndreAguirreRoman <ankar_aguirre@live.com>
2021-08-12 16:09:11 -0700

    Added the port variable

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
 
+
 app = Flask(__name__)
 
 
@@ -519,6 +520,8 @@ def variation_delete(id):
 
     return 'Variation successfully deleted'
 
+
+port = int(os.getenv('PORT'))
 
 if __name__ == '__main__':
     app.run(debug=True)
